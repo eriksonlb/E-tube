0b084e03d4ecc2eee53247515092e815fe628c89
Lucas Silva <69327287+lucas-Dk@users.noreply.github.com>
2020-10-18 16:56:26 -0300

    Update e-tube.py

--- e-tube.py
+++ e-tube.py
@@ -11,13 +11,14 @@ while True:
 |   __|  |______|    |  |     |  |  |  | |   _  <  |   __|  
 |  |____             |  |     |  `--'  | |  |_)  | |  |____ 
 |_______|            |__|      \______/  |______/  |_______|
-															 \033[m""")
+			
+				V 1.1												 \033[m""")
 	print("\nOpção em vermelho não está funcionando no momento!")
 	print("""
 Menu:
 
 [1] - Baixar um vídeo MP4
-\033[1;31m[2] - Baixar uma playlist MP4\033[m
+[2] - Baixar uma playlist MP4
 \033[1;31m[3] - Baixar uma música MP3\033[m
 [x] - Sair
 
@@ -30,7 +31,7 @@ Menu:
 		
 		if user == 1:
 
-			print("Opção de baixar apenas um vídeo está executando...")
+			print("Opção de baixar apenas um vídeo está executando...\n")
 
 			link = str(input("Url do vídeo: "))
 			print("Buscando streams...")
@@ -52,12 +53,29 @@ Menu:
 			suspender(1.3)
 
 		elif user == 2:
+			print("Opção de baixar uma playlist está executando...\n")
 
-			print("Opção de baixar uma playlist ainda não está disponível!")
-			print("Ficará disponível em breve!")
-			print("Voltando ao menu...")
-			suspender(1.3)
+			print("\033[1;31mOBS: Confira se nenhum link está quebrado para que o download ocorra corretamente!\033[m\n")
+			print("\033[1;4mO que acontece se tiver um link quebrado?\033[m\n")
+			print("Ele vai baixando video por vídeo, quando chegar no video")
+			print("coma a url quebrada, ele para de baixar os vídeos!\n")
+			input("Se entendeu dê enter para continuar\n")
+			link_play = str(input("Url da playlist: "))
+			PLAYLIST_URL = link_play
+			playlist = Playlist(PLAYLIST_URL)
+			print("\n\033[1;32mBaixando sua playlist!")
+			print("Isso pode demorar um pouco...\033[m\n")
+			for url in playlist:
+				video = YouTube(url)
+				stream = video.streams.get_highest_resolution()
+				try:
+					stream.download(output_path='playlist')
+				except:
+					pass
+				else:
+					pass
 
+			print("Download Completo! Verifique na pasta!")
 
 		elif user == 3:
 
